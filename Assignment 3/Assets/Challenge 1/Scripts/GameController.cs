c1eb12fc47a0a702c7a4793d8212f9a1621a756b
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2019-09-11 00:46:56 -0500

    The entire project is done

---
+++ Assignment 3/Assets/Challenge 1/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    public void AddScore(int scoreValue)
+    {
+        score += scoreValue;
+        Debug.Log(score);
+    }
+}
---
+++ Assignment 3/Assets/Challenge 1/Scripts/ScoreTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour
+{
+
+    public int scoreValue;
+    private GameController gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            gameController.AddScore(scoreValue);
+        }
+    }
+}
